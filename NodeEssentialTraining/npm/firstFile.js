9fc03723d06c32ac61b1b5e50a885ebdf2ab7722
johncban <johncban@gmail.com>
2021-05-16 03:38:57 -0400

    Upload Progress Exercise

---
+++ NodeEssentialTraining/npm/firstFile.js
@@ -0,0 +1,5 @@
+let hello = "Hello World from NODE JS";
+
+console.log("Hello World");
+
+globalThis.console.log(hello);
---
+++ NodeEssentialTraining/npm/globalProcess.js
@@ -0,0 +1,23 @@
+const grab = flag => {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1
+    return process.argv[indexAfterFlag]
+}
+
+
+
+const greeting = grab("--greeting")
+const user = grab("--user")
+
+
+console.log(`${greeting} ${user}`)
+
+
+/*
+console.log(process.argv)
+console.log(process.pid)
+console.log(process.versions.node)
+
+
+const [, , firstName, lastName] = process.argv;
+console.log(`Your name is ${firstName} ${lastName}`)
+*/
